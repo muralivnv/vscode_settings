394833f8cd70ffe295c8e56701eefe1859303a57
muralivnv <muralivnv@gmail.com>
2020-11-05 23:33:46 -0500

    updated color scheme and added ignore folders option for rg

--- fuzzy_search.py
+++ fuzzy_search.py
@@ -6,17 +6,22 @@ search_type = "--search-file-content"
 if len(argv) > 1:
   search_type = argv[1]
 
+#################################################################
+folders_to_ignore = ["CMakeFiles"]
+
 def search_inside_files():
-  return "rg --line-number \"\\w*\" | fzf"
+  ignore_glob = ''.join(["--iglob \"!**/{}/**\" ".format(f) for f in folders_to_ignore])
+  return "rg --line-number {0} \"\\w*\" | fzf --color hl:221,hl+:74".format(ignore_glob)
 
 def search_filenames():
-  return "fzf"
+  return "fzf --color hl:221,hl+:74 "
+  
+#################################################################
 
 cmd = {}
 cmd["--search-file-content"] = search_inside_files
 cmd["--search-files"]        = search_filenames
 
-#%%
 try:
   file = check_output(cmd[search_type](), shell=True)
   file = file.decode("utf-8")
